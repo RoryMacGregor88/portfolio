1841a0d4d7f5128d691951df00ab74c6bdb16214
Rory MacGregor <rory.macgregor@astrosat.net>
2023-06-27 15:22:39 +0100

    feat(frontend): Component composition for re-use, custom intro panel
    Split up the main section elements into smaller components, so that
    the intro panel could be composed to re-use some of them but not all.

--- src/components/project-display/project-display.component.tsx
+++ src/components/project-display/project-display.component.tsx
@@ -8,8 +8,6 @@ import { Button } from '~/components';
 import ChardDashboardImage from '~/images/chart-dashboard.png';
 import FaceImage from '~/images/face.jpg';
 
-// TODO: responsive stuff, proper theming
-
 type Project = {
   name: string;
   descriptions: string[];
@@ -19,24 +17,6 @@ type Project = {
 };
 
 const PROJECT_DATA: Project[] = [
-  {
-    name: `Hi, I'm Rory.`,
-    descriptions: [
-      `I'm a 34-year-old full-stack JavaScript developer based in Edinburgh, Scotland. I've been involved in software development for 6 years and working professionally for the last 4 years at an aerospace firm in Edinburgh called Astrosat, which specialised in visualising satellite data on maps and creating custom tools with which to interact with it.`,
-      'Scrolling down this page, you will find short descriptions of projects that demonstrate my knowledge and abilities, as well as links to standalone deploys and repositories for each.',
-    ],
-    src: ChardDashboardImage,
-    buttonMetadata: [
-      {
-        label: 'View LinkedIn',
-        href: 'www.linkedIn.com',
-      },
-      {
-        label: 'View GitHub',
-        href: 'www.github.com',
-      },
-    ],
-  },
   {
     name: '1. Chart Dashboard',
     descriptions: [
@@ -112,7 +92,7 @@ const PROJECT_DATA: Project[] = [
   {
     name: '5. This site!',
     descriptions: [
-      `Yes, this very portfolio app was created using Next.js 13, Typescript and Tailwind CSS.`,
+      `Yes, everything about this portfolio app was created manually using Next.js 13, Typescript and Tailwind CSS.`,
       `I'm relatively new to Tailwind, so I thought this would serve as an opportunity to try it out. It's fully responsive, so feel free to have a play around.`,
     ],
     responsive: true,
@@ -130,52 +110,139 @@ const PROJECT_DATA: Project[] = [
   },
 ];
 
-interface ListProps {
+const List = ({ children }: { children: ReactNode }) => <ul>{children}</ul>;
+
+interface SectionWrapperProps {
+  isEven: boolean;
   children: ReactNode;
 }
 
-const List = ({ children }: ListProps) => <ul>{children}</ul>;
+/** container comprising black text and yellow image sections */
+const SectionWrapper = ({ isEven, children }: SectionWrapperProps) => (
+  <li
+    className={`flex flex-col ${
+      isEven ? 'lg:flex-row' : 'lg:flex-row-reverse'
+    } justify-between align-middle lg:h-screen lg:pl-12`}
+  >
+    {children}
+  </li>
+);
 
-interface SectionProps {
+/** black panel with text and buttons */
+const ContentArea = ({ children }: { children: ReactNode }) => (
+  <div className='bg-black lg:w-1/2 flex flex-col justify-center text-center py-8 lg:py-0'>
+    {children}
+  </div>
+);
+
+const Title = ({ children }: { children: ReactNode }) => (
+  <h2 className='text-4xl lg:text-6xl font-bold'>{children}</h2>
+);
+
+/** paragraphs in between title and buttons */
+const DescriptionSection = ({ children }: { children: ReactNode }) => (
+  <div className='m-6 text-lg lg:text-2xl flex flex-col gap-2'>{children}</div>
+);
+
+const ButtonSection = ({ children }: { children: ReactNode }) => (
+  <div className='flex gap-4 mx-5 justify-between'>{children}</div>
+);
+
+/** yellow panel with images */
+const ImageArea = ({ children }: { children: ReactNode }) => (
+  <div className='bg-yellow lg:w-1/2 flex flex-col justify-center'>
+    {children}
+  </div>
+);
+
+/** first black/yellow panel that is unique from the others */
+const IntroSection = () => (
+  <SectionWrapper isEven={true}>
+    <ContentArea>
+      <Title>Hi, I&apos;m Rory.</Title>
+      <DescriptionSection>
+        <p>
+          I&apos;m a 34-year-old full-stack JavaScript developer based in
+          Edinburgh, Scotland. I&apos;ve been involved in software development
+          for 6 years and working professionally for the last 4 years at an
+          aerospace firm in Edinburgh called Astrosat, which specialised in
+          visualising satellite data on maps and creating custom tools with
+          which to interact with it.
+        </p>
+        <p>
+          Scrolling down this page, you will find short descriptions of projects
+          that demonstrate my knowledge and abilities, as well as links to
+          standalone deploys and repositories for each.
+        </p>
+      </DescriptionSection>
+
+      <ButtonSection>
+        <NextLink href={'www.google.com'} style={{ width: '100%' }}>
+          <Button label={'View LinkedIn'} className='lg:p-4' />
+        </NextLink>
+        <NextLink href={'www.google.com'} style={{ width: '100%' }}>
+          <Button label={'View Github'} className='lg:p-4' />
+        </NextLink>
+      </ButtonSection>
+    </ContentArea>
+
+    <ImageArea>
+      <div
+        style={{
+          border: '8px solid #fff',
+          borderRadius: '50%',
+          margin: '10rem',
+          overflow: 'hidden',
+          objectFit: 'contain',
+        }}
+      >
+        <NextImage
+          alt={`Rory MacGregor headshot`}
+          src={FaceImage}
+          style={{ objectFit: 'fill' }}
+          placeholder='blur'
+        />
+      </div>
+    </ImageArea>
+  </SectionWrapper>
+);
+
+interface ProjectSectionProps {
   project: Project;
   index: number;
 }
 
-const Section = ({
+const ProjectSection = ({
   project: { descriptions, responsive, src, name, buttonMetadata },
   index,
-}: SectionProps) => {
+}: ProjectSectionProps) => {
   const isEven = index % 2 === 0;
   return (
-    <li
-      className={`flex flex-col ${
-        isEven ? 'lg:flex-row' : 'lg:flex-row-reverse'
-      } justify-between align-middle lg:h-screen lg:pl-12`}
-    >
-      <div className='bg-black lg:w-1/2 flex flex-col justify-center text-center py-8 lg:py-0'>
-        <h2 className='text-4xl lg:text-6xl font-bold'>{name}</h2>
+    <SectionWrapper isEven={isEven}>
+      <ContentArea>
+        <Title>{name}</Title>
 
-        <div className='m-6 text-lg lg:text-2xl flex flex-col gap-2'>
+        <DescriptionSection>
           {descriptions.map((paragraph) => (
             <p key={paragraph}>{paragraph}</p>
           ))}
-        </div>
+        </DescriptionSection>
 
-        <div>
+        <>
           <h4 className='mb-6 text-xl lg:text-3xl'>
             Screen size: {responsive ? 'Responsive' : 'Desktop'}
           </h4>
-          <div className='flex gap-4 mx-5 justify-between'>
+          <ButtonSection>
             {buttonMetadata.map(({ label, href }) => (
               <NextLink key={label} href={href} style={{ width: '100%' }}>
-                <Button label={label} />
+                <Button label={label} className='lg:p-4' />
               </NextLink>
             ))}
-          </div>
-        </div>
-      </div>
+          </ButtonSection>
+        </>
+      </ContentArea>
 
-      <div className='bg-yellow lg:w-1/2 flex flex-col justify-center'>
+      <ImageArea>
         <div className='border-8 border-solid border-white rounded-md overflow-hidden mx-8 lg:mx-16 my-8 lg:my-0'>
           <NextImage
             alt={name}
@@ -184,15 +251,16 @@ const Section = ({
             placeholder='blur'
           />
         </div>
-      </div>
-    </li>
+      </ImageArea>
+    </SectionWrapper>
   );
 };
 
 const ProjectDisplay = () => (
   <List>
+    <IntroSection />
     {PROJECT_DATA.map((project, i) => (
-      <Section key={project.name} index={i} project={project} />
+      <ProjectSection key={project.name} index={i + 1} project={project} />
     ))}
   </List>
 );
