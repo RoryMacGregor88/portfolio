63a98b20609aaf872a486aec035a82981e90d9b3
Rory MacGregor <rory.macgregor@astrosat.net>
2023-08-24 14:42:12 +0100

    feat(frontend): Change name of circle-image file (misspelled)

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,7 @@ import Button from './button/button.component';
 import ExternalLink from './external-link.component';
 import ContactForm from './contact-form/contact-form.component';
 import TechIconList from './tech-icon-list.component';
-import CircleImage from './cirlce-image.component';
+import CircleImage from './circle-image.component';
 import AutoScrollButtons from './auto-scroll-buttons.component';
 
 /** shared styled components used to compose sections */
